68723e060efdbd3deb9402e9f2a7cc6b860f2c56
Joonas Kinnunen <joonas.kinn@gmail.com>
2021-05-09 22:12:20 +0300

    Add solution to part 2 exercise 1

---
+++ osa02-Osa02_01.HelloThymeleaf/src/main/java/hellothymeleaf/HelloThymeleafController.java
@@ -0,0 +1,22 @@
+package hellothymeleaf;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloThymeleafController {
+
+    @Controller
+    public class ThymeleafController {
+
+        @GetMapping("/")
+        public String home() {
+            return "index";
+        }
+        
+        @GetMapping("/video")
+        public String video() {
+            return "video";
+        }
+    }
+}
