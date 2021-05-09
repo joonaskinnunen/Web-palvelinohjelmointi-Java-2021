14ee09e9598abd6bb0daf0cddbce50256dbc4b0a
Joonas Kinnunen <joonas.kinn@gmail.com>
2021-05-09 20:36:20 +0300

    Add solutions to exercises 8 and 9

--- osa01-Osa01_09.HelloRequestParams/src/main/java/hellorequestparams/HelloRequestParamsController.java
+++ osa01-Osa01_09.HelloRequestParams/src/main/java/hellorequestparams/HelloRequestParamsController.java
@@ -9,4 +9,24 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HelloRequestParamsController {
 
+    @GetMapping("/hello")
+    @ResponseBody
+    public String tervehdi(@RequestParam String param) {
+        return "Hello " + param;
+    }
+    
+    @GetMapping("/params")
+    @ResponseBody
+    public String returnParams(@RequestParam Map<String, String> parametrit) {
+        String str = "{";
+        Object[] keys = parametrit.keySet().toArray();
+        Object[] values = parametrit.values().toArray();
+        
+        for(int i = 0; i < parametrit.size(); i++) {
+            str += keys[i].toString() + "=" + values[i].toString();
+            if(i < parametrit.size() - 1) str += ", ";
+        }
+        str += "}";
+        return str;
+    }
 }
